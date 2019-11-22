04bc5e14ff6a1dca6815d8f6341fe2bae431ac90
modeware <safiqlodi2@gmail.com>
2019-11-22 21:58:39 +0530

    Updates

---
+++ precache-manifest.f77c89a56e6e611351c7dabafae1b1fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e24ade36ffcfdd7f5f60f588a51a2c",
+    "url": "/safiq-s-monsters/index.html"
+  },
+  {
+    "revision": "8efe7a85abb1da711773",
+    "url": "/safiq-s-monsters/static/css/main.e70cc1e5.chunk.css"
+  },
+  {
+    "revision": "d145b5e0fa33e7f931d2",
+    "url": "/safiq-s-monsters/static/js/2.5b4d1b28.chunk.js"
+  },
+  {
+    "revision": "8efe7a85abb1da711773",
+    "url": "/safiq-s-monsters/static/js/main.ff91c2a6.chunk.js"
+  },
+  {
+    "revision": "6e36916c7e5475950c78",
+    "url": "/safiq-s-monsters/static/js/runtime~main.275f5c14.js"
+  }
+]);
